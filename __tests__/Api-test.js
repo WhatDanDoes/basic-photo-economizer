306fb9e1bbd083e9bf7e0b661dd769f2f8f5db50
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-06-27 20:02:12 -0600

    Get incipient API tests

---
+++ __tests__/Api-test.js
@@ -0,0 +1,54 @@
+/**
+ * @format
+ */
+
+//import 'react-native';
+//import React from 'react';
+import Api from '../lib/Api';
+
+import moxios from 'moxios';
+
+//import Enzyme from 'enzyme';
+//import Adapter from 'enzyme-adapter-react-16';
+//Enzyme.configure({ adapter: new Adapter() });
+
+
+// Note: test renderer must be required after react-native.
+//import renderer from 'react-test-renderer';
+//
+
+it('doesn\'t bomb', () => {
+  expect(Api).toBeDefined();
+  expect(Object.keys(Api).length).toEqual(1);
+});
+ 
+describe('postImage', () => {
+  beforeEach(() => {
+    moxios.install()
+
+    moxios.stubRequest(/.*/, {
+      status: 201,
+      responseText: 'hello'
+    });
+  });
+
+  afterEach(() => {
+    moxios.uninstall()
+  });
+
+ 
+  it('sends a properly formatted API call', async () => {
+    let result;
+    try {
+      result = await Api.postImage('somelongbase64imagestring');
+    }
+    catch (err) {
+      throw err;
+    }
+    expect(result.status).toEqual(201);
+
+    let request = moxios.requests.mostRecent();
+    expect(request.url).toEqual('https://example.com/image');
+    expect(request.config.method).toEqual('post');
+  });
+});
