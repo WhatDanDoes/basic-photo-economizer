95cd26d05659c1a51887c708e97e2b2158a0f5f1
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-11 14:00:34 -0600

    Get login API

--- __tests__/Api-test.js
+++ __tests__/Api-test.js
@@ -19,7 +19,7 @@ import moxios from 'moxios';
 
 it('doesn\'t bomb', () => {
   expect(Api).toBeDefined();
-  expect(Object.keys(Api).length).toEqual(1);
+  expect(Object.keys(Api).length).toEqual(2);
 });
  
 describe('postImage', () => {
@@ -52,4 +52,42 @@ describe('postImage', () => {
     expect(request.config.method).toEqual('post');
   });
 });
+
+
+describe('login', () => {
+  beforeEach(() => {
+    moxios.install()
+
+    moxios.stubRequest(/.*/, {
+      status: 201,
+      responseText: 'hello'
+    });
+  });
+
+  afterEach(() => {
+    moxios.uninstall()
+  });
+
+  it('sends a properly formatted API call', async () => {
+    let result;
+    try {
+      result = await Api.login({ email: 'someguy@example.com', password: 'secret' });
+    }
+    catch (err) {
+      throw err;
+    }
+    expect(result.status).toEqual(201);
+
+    let request = moxios.requests.mostRecent();
+    console.log(request.config);
+    expect(request.url).toEqual('https://localhost:3001/login');
+    expect(request.config.method).toEqual('post');
+    let credentials = JSON.parse(request.config.data);
+    expect(credentials.email).toEqual('someguy@example.com');
+    expect(credentials.password).toEqual('secret');
+    expect(request.config.headers['Accept']).toMatch(/json/);
+    expect(request.config.headers['Content-Type']).toMatch(/json/);
+  });
+});
+
 
