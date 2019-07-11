fb4790227f7be2448c0e1841784902a0a096763e
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-11 12:28:11 -0600

    Get better description

---
+++ e2e/authentication.spec.js
@@ -0,0 +1,92 @@
+describe('authentication', () => {
+
+  // 2019-6-25 https://labs.chiedo.com/post/async-mocha-tests/
+  var mochaAsync = (fn) => {
+    return done => {
+      fn.call().then(done, err => {
+        done(err);
+      });
+    };
+  };
+
+  describe('login form', () => {
+    beforeEach(mochaAsync(async () => {
+      await device.reloadReactNative();
+    }));
+
+    it('has a login form on the screen', mochaAsync(async () => {
+      await expect(element(by.id('login-form'))).toBeVisible();
+      await expect(element(by.id('email-input'))).toBeVisible();
+      await expect(element(by.id('password-input'))).toBeVisible();
+      await expect(element(by.id('login-button'))).toBeVisible();
+    }));
+  
+//    it('has a take-picture-button button on screen', async () => {
+//      await expect(element(by.id('take-picture-button'))).toBeVisible();
+//    });
+//  
+//    it('does not have a back-button on the screen', async () => {
+//      await expect(element(by.id('back-button'))).toBeNotVisible();
+//    });
+  });
+
+
+//  describe('take-picture-button', () => {
+//    beforeEach(mochaAsync(async () => {
+//      await device.reloadReactNative();
+//    }));
+//
+//    it('displays the image captured and hides the camera', mochaAsync(async () => {
+//      await expect(element(by.id('camera'))).toBeVisible();
+//      await element(by.id('take-picture-button')).tap();
+//      await expect(element(by.id('camera'))).toBeNotVisible();
+//      await expect(element(by.id('image-preview'))).toBeVisible();
+//    }));
+//
+//    it('shows the back-button and hides the take-picture-button', mochaAsync(async () => {
+//      await expect(element(by.id('back-button'))).toBeNotVisible();
+//      await element(by.id('take-picture-button')).tap();
+//      await expect(element(by.id('back-button'))).toBeVisible();
+//      await expect(element(by.id('take-picture-button'))).toBeNotVisible();
+//    }));
+//
+//    it('shows the send-button', mochaAsync(async () => {
+//      await expect(element(by.id('send-button'))).toBeNotVisible();
+//      await element(by.id('take-picture-button')).tap();
+//      await waitFor(element(by.id('send-button'))).toBeVisible();
+//      // A constant reminder that I don't fully know what I'm doing
+////      await expect(element(by.id('send-button'))).toBeVisible();
+//    }));
+//
+//    describe('back-button', () => {
+//      beforeEach(mochaAsync(async () => {
+//        await device.reloadReactNative();
+//
+//        await element(by.id('take-picture-button')).tap()
+//        await waitFor(element(by.id('back-button'))).toBeVisible().withTimeout(2000);
+//        await waitFor(element(by.id('image-preview'))).toBeVisible();
+//      }));
+//
+//      it('shows the take-picture-button and hides the back-button', mochaAsync(async () => {
+//        await expect(element(by.id('take-picture-button'))).toBeNotVisible();
+//        await element(by.id('back-button')).tap();
+//        await waitFor(element(by.id('back-button'))).toBeNotVisible();
+//        await expect(element(by.id('take-picture-button'))).toBeVisible();
+//      }));
+//
+//      it('shows the camera and hides the image-preview', mochaAsync(async () => {
+//        await expect(element(by.id('image-preview'))).toBeVisible();
+//        await expect(element(by.id('camera'))).toBeNotVisible();
+//        await element(by.id('back-button')).tap();
+//        await waitFor(element(by.id('image-preview'))).toBeNotVisible();
+//        await expect(element(by.id('camera'))).toBeVisible();
+//      }));
+//
+//      it('hides the send-button', mochaAsync(async () => {
+//        await expect(element(by.id('send-button'))).toBeVisible();
+//        await element(by.id('back-button')).tap();
+//        await expect(element(by.id('send-button'))).toBeNotVisible();
+//      }));
+//    });
+//  });
+});
--- e2e/send.spec.js
+++ e2e/send.spec.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('send', () => {
 
   // 2019-6-25 https://labs.chiedo.com/post/async-mocha-tests/
   var mochaAsync = (fn) => {
