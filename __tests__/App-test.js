da0b3b3d71205b7b42f90f943d4b49e140c99fb8
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-13 09:20:37 -0600

    Get primitive App state test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,9 +6,19 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('sets state.image to null', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state('image')).toBeNull();
 });
